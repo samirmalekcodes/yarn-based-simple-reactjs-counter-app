38d02a44a42924050ebf6c8522bcd34b589ab66d
samirmalekcodes <samirmalek.dev@gmail.com>
2021-12-23 02:45:24 +0300

    finished app

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import {useState} from "react";
 // STATE is a variable when information is updated directly displayes it on the screen
 
 function App() {
-  const [count, setCount] = useState(25); // STATE Variable
+  const [count, setCount] = useState(30); // STATE Variable
 
   // increment a state variable  -> setCount(count + 1)
   // versus
   // increment a REGULAR  variable -> myAge = myAge + 1
   
-  const myAge = 25; // regular JS variable
+  // const myAge = 25; // regular JS variable
   return (
     <div className="App">
-      <h2 style={{color: "red"}}> {myAge} </h2>
-      <button onClick={() => alert("You just clicked + button")}> + </button>
-      <button onClick={() => alert("You just clicked - button")}> - </button>
+      <h2 style={{color: "red"}}> {count} </h2>
+      <button onClick={() => {setCount(count + 1 )}}> + </button>
+      <button onClick={() => {setCount(count - 1)}}> - </button>
     </div>
   );
 }
